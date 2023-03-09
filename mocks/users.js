da790d05d7330f28c1ef11f0bfb8932adfea0c87
Matheus Aurélio <matheusaurelio2004@gmail.com>
2023-03-09 01:51:41 -0300

    :sparkles: feat: Create mock list users

---
+++ mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: "Matheus Aurélio"
+    },
+    {
+        id: 2,
+        name: "Ronaldo Souza"
+    },
+    {
+        id: 3,
+        name: "Ana Maria"
+    }
+]
